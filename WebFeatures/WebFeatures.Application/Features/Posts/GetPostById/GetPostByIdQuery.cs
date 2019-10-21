dd5acc25ede6af9293690c88540a8f03561415f3
Vyacheslav Chuletc <chuletsva@gmail.com>
2019-10-21 20:51:34 +0200

    WebFeatures/QueryFiltering moved into own repositories

--- WebFeatures/WebFeatures.Application/Features/Posts/GetPostById/GetPostByIdQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebFeatures.Application.Infrastructure.Pipeline.Abstractions;
-
-namespace WebFeatures.Application.Features.Posts.GetPostById
-{
-    public class GetPostByIdQuery : IQuery<PostEditDto>
-    {
-        public int Id { get; set; }
-    }
-}
